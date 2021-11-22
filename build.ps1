63c94a63785a15edfc68ef89430f1e02a1d2d35f
Neko Boi Nick <thakyz@outlook.com>
2021-11-22 10:34:55 -0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,4 +1,3 @@
-# Test comment
 New-Item -Path "$env:GITHUB_WORKSPACE\src" -ItemType Directory
 New-Item -Path "$env:GITHUB_WORKSPACE\src\bin" -ItemType Directory
 New-Item -Path "$env:GITHUB_WORKSPACE\src\bin\built.txt" -ItemType File
